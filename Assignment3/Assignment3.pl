cc982fc2d44b3d59d9d1fccd27ca8bd70df1f4c5
Alex Khawalid <a.khawalid@gmail.com>
2015-04-16 16:50:58 +0200

    spul
    enzo

--- Assignment3/Assignment3.pl
+++ Assignment3/Assignment3.pl
@@ -5,6 +5,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Operators
+:- dynamic fact/1.
 :- op(800, fx, if).
 :- op(700, xfy, then).
 :- op(300, xfy, or).
@@ -26,18 +27,30 @@ if piek_koorts then malaria_goedaardig.
 if (malaria_tropica or malaria_goedaardig) then malaria.
 
 % Darminfecties
-if diarree then diarree.
 if (diarree or misselijk or buikpijn) then giardiasis.
 if (koorts or kramp or ontlasting) then bascillaire_dysenterie.
-if hoge_koorts then tyfus.
-if (hoge_koorts or diarree or kweek) then paratyfus.
-if (braken or diarree) then salmonellosen.
+if verlammings_verschijnselen then kinderverlamming.
 if (gele_huid or misselijkheid or donkere_urine) then geelzucht.
 if dunne_diaree then cholera.
 
 %%%%%%%%%%%%%%%%%%
 % Program
 %%%%%%%%%%%%%%%%%%
+start:-
+    write('Heeft u hoofdpijn? Graag antwoorden met ja of nee.\n'),
+    read(Symptom),
+    if (Symptom is ja) then deriveDisease(hoofdpijn,Diseases,RulesUsed).
+
+deriveDisease(Symptom,Diseases,RulesUsed):-
+    assert(fact(Symptom)),
+    forward.
+    %getDiseases(Symptom,Diseases).
+
+getDiseases(Symptom,PossibleDiseases):-
+    bagof(Y, if Symptom then Y, PossibleDiseases );
+    bagof(Y, if Symptom or _ then Y, PossibleDiseases);
+    bagof(Y, if _ or Symptom then Y, PossibleDiseases);
+    bagof(Y, if _ or Symptom or _ then Y, PossibleDiseases).
 
 test(A, B) :-
 	if A then B.
